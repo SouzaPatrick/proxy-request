22017f14df9f8ebbee35dc4310b1844a3e9f619b
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-02-26 23:52:28 -0300

    feat: Add class to perform requests from public or private proxies

---
+++ proxy_request.py
@@ -0,0 +1,46 @@
+import requests
+from typing import Optional
+
+
+class ProxyRequest:
+    def __init__(self, timeout: int = 10, destination_host: str = '', destination_host_port: int = 80) -> None:
+        '''
+        Use public or private proxy to simulate that your access comes from another country
+        :param destination_host: Destination host IP or URL
+        :param destination_host_port: Port of the application that will be the target
+        :param timeout: Maximum waiting time for request return
+        '''
+        self.destination_host: str = destination_host
+        self.timeout: int = timeout
+
+        if self.destination_host:
+            self.destination_host += str(destination_host_port)
+
+    def _get_session(self, host_and_port: str, protocol: str) -> requests.Session:
+        '''
+        Construct an HTTP session
+        :param host_and_port: HOST_IP:PORT of the proxy that will be used in the request attempt
+        :param protocol: http_https | sock4 | sock5
+        :return: Response from the request
+        '''
+        session: requests.Session = requests.Session()
+        if protocol != 'http_https':
+            session_host_and_port: str = f'{protocol}://{host_and_port}'
+            session.proxies = {"http": session_host_and_port, "https": session_host_and_port}
+        else:
+            session.proxies = {"https": host_and_port, "http": host_and_port}
+
+        return session
+
+    def request(self, host_and_port: str, protocol: str = 'http_https') -> Optional[requests.Response]:
+        '''
+        :param host_and_port: HOST_IP:PORT of the proxy that will be used in the request attempt
+        :param protocol: http_https | sock4 | sock5
+        :return: Response from the request
+        '''
+        if self.destination_host:
+            session = self._get_session(host_and_port, protocol)
+            response: Optional[requests.Response] = session.get(self.destination_host, timeout=self.timeout)
+        else:
+            response: Optional[requests.Response] = None
+        return response
---
+++ tests/test_proxy_request.py
@@ -0,0 +1,39 @@
+from proxy_request import ProxyRequest
+from typing import NoReturn
+import pytest
+import requests
+
+
+@pytest.fixture
+def proxy_request() -> ProxyRequest:
+    _proxy_request: ProxyRequest = ProxyRequest()
+    return _proxy_request
+
+
+def test_init_default_values(proxy_request) -> NoReturn:
+    assert proxy_request.destination_host == ''
+    assert proxy_request.timeout == 10
+
+
+def test_request_not_send_url(proxy_request) -> NoReturn:
+    assert proxy_request.request(host_and_port='0.0.0.0:80') is None
+
+
+@pytest.mark.parametrize(
+    'protocol',
+    [
+        'sock4',
+        'sock5'
+    ]
+)
+def test_get_session_without_sending_the_protocol_as_http(proxy_request, protocol) -> NoReturn:
+    session: requests.Session = proxy_request._get_session(host_and_port='0.0.0.0:80', protocol=protocol)
+    assert session.proxies == {"http": f'{protocol}://0.0.0.0:80', "https": f'{protocol}://0.0.0.0:80'}
+
+
+def test_get_session_sending_the_protocol_as_http(proxy_request) -> NoReturn:
+    session: requests.Session = proxy_request._get_session(host_and_port='0.0.0.0:80', protocol='http_https')
+    assert session.proxies == {"https": '0.0.0.0:80', "http": '0.0.0.0:80'}
+
+
+
