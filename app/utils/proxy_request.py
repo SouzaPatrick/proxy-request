652b7d8b09b97389bcc39d45442dd674502db5ce
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-03-11 17:20:02 -0300

    test: Add exception error tests

--- app/utils/proxy_request.py
+++ app/utils/proxy_request.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import requests
-from requests.exceptions import ProxyError, Timeout
 from rich.console import Console
 
 from app.utils.check_proxy_request import CheckProxyRequest
@@ -16,13 +15,16 @@ def proxy_request(proxy: str, protocol: str = "http") -> bool:
         response: Optional[requests.Response] = CheckProxyRequest(
             destination_host=DESTINATION_HOST
         ).request(proxy=proxy, protocol=protocol)
+
         if response:
             proxy_status_check: bool = True
         else:
             proxy_status_check: bool = False
-    except ProxyError:
+    except requests.exceptions.ProxyError:
         proxy_status_check: bool = False
-    except Timeout:
+    except requests.exceptions.ConnectionError:
+        proxy_status_check: bool = False
+    except requests.exceptions.Timeout:
         proxy_status_check: bool = False
 
     if proxy_status_check:
--- tests/utils/test_proxy_request.py
+++ tests/utils/test_proxy_request.py
@@ -1,4 +1,6 @@
-from app.utils.check_proxy_request import CheckProxyRequest
+import pytest
+import requests
+
 from app.utils.proxy_request import proxy_request
 
 
@@ -15,6 +17,39 @@ def test_proxy_request_when_the_check_return_None(mocker):
     mocker.patch(
         "app.utils.check_proxy_request.CheckProxyRequest.request", return_value=None
     )
+    proxy_status_check: bool = proxy_request(proxy="")
+
+    assert proxy_status_check is False
+
+
+def test_proxy_error(mocker):
+    mocker.patch(
+        "app.utils.check_proxy_request.CheckProxyRequest.request",
+        side_effect=requests.exceptions.ProxyError,
+    )
+
+    proxy_status_check: bool = proxy_request(proxy="")
+
+    assert proxy_status_check is False
+
+
+def test_proxy_connection_error(mocker):
+    mocker.patch(
+        "app.utils.check_proxy_request.CheckProxyRequest.request",
+        side_effect=requests.exceptions.ConnectionError,
+    )
+
+    proxy_status_check: bool = proxy_request(proxy="")
+
+    assert proxy_status_check is False
+
+
+def test_proxy_timeout(mocker):
+    mocker.patch(
+        "app.utils.check_proxy_request.CheckProxyRequest.request",
+        side_effect=requests.exceptions.Timeout,
+    )
+
     proxy_status_check: bool = proxy_request(proxy="")
 
     assert proxy_status_check is False
