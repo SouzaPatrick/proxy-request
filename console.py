14e4eab42dcaafb607d40b342a8e54111a379822
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-03-12 18:47:09 -0300

    refactor: Changing the context to run the tests on the database

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ from rich.table import Table
 
 from app.db_functions import get_protocol_with_id, get_valid_proxies
 from app.models import Proxy
+from database import get_session
 
 table = Table(title="Table of valid proxies", box=box.SIMPLE)
 table.add_column("Nº")
@@ -13,13 +14,19 @@ table.add_column("Url")
 table.add_column("Protocol")
 table.add_column("Last check")
 
-proxies: Optional[list[Proxy]] = get_valid_proxies()
+with get_session() as session:
+    proxies: Optional[list[Proxy]] = get_valid_proxies(session=session)
+
 for index, proxy in enumerate(proxies):
+    with get_session() as session:
+        protocol_name: str = get_protocol_with_id(
+            session=session, protocol_id=proxy.extraction_method.protocol_id
+        ).name
     table.add_row(
         str(index),
         f"{proxy.ip}:{proxy.port}",
         proxy.extraction_method.url,
-        get_protocol_with_id(proxy.extraction_method.protocol_id).name,
+        protocol_name,
         proxy.last_check.strftime("%d/%m/%Y %H:%M:%S"),
     )
 print(table)
