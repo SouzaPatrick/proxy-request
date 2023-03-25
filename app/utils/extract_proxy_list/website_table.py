db7a4f353aa7b4432e8d30eda213acdd7b8bfe3a
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-03-25 18:45:46 -0300

    test: Add test_extract_proxy_list_success

--- app/utils/extract_proxy_list/website_table.py
+++ app/utils/extract_proxy_list/website_table.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup as bs
 
 def extract_proxy_list(url: str) -> list[str]:
     # get the HTTP response and construct soup object
-    soup = bs(requests.get(url).content, "html.parser")
+    response = requests.get(url)
+    soup = bs(response.content, "html.parser")
     proxies: list[str] = []
     for row in soup.find("table", attrs={"class": "table"}).find_all("tr")[1:]:
+
         tds = row.find_all("td")
         try:
             ip: str = tds[0].text.strip()
