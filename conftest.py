5316a9ce19e68eea20d5aa6bd8f0ede80be9c054
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-03-26 18:33:42 -0300

    test: Add fixtures

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 import pytest
 from sqlmodel import Session, SQLModel
 
 from app import models
+from tests._factories import ExtractionMethodFactory, ProtocolFactory, ProxyFactory
 from tests.database import engine
 
 
-@pytest.fixture()
+@pytest.fixture
 def db_setup():
 
     """Creates a database engine in memory."""
@@ -23,3 +26,36 @@ def session_fixture(db_setup):
     """
     with Session(engine) as session:
         yield session
+
+
+@pytest.fixture
+def protocol() -> models.Protocol:
+    protocol_factory: models.Protocol = ProtocolFactory(name="fake_protocol")
+    return protocol_factory
+
+
+@pytest.fixture
+def extraction_method(protocol) -> models.ExtractionMethod:
+    extraction_method_factory: models.ExtractionMethod = ExtractionMethodFactory(
+        name="fake_extraction_method",
+        url="http://test.com",
+        priority=0,
+        method="fake_method",
+        protocol_id=protocol.id,
+        protocol=protocol,
+    )
+    return extraction_method_factory
+
+
+@pytest.fixture
+def proxy(extraction_method) -> models.Proxy:
+    proxy_factory: models.Proxy = ProxyFactory(
+        ip="0.0.0.0",
+        port=80,
+        status_check=True,
+        ttl=30,
+        last_check=datetime.now(),
+        extraction_method_id=extraction_method.id,
+        extraction_method=extraction_method,
+    )
+    return proxy_factory
