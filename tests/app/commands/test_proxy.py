93a3ac0bcf00cdaa935544aa5d93e3227fe266f2
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-05-15 08:20:20 -0300

    test: Add test for proxy command

--- tests/app/commands/test_proxy.py
+++ tests/app/commands/test_proxy.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 
+from freezegun import freeze_time
 from sqlmodel import Session, select
 
-from app.commands.proxy import create_proxies
+from app.commands.proxy import create_proxies, proxy
 from app.models import ExtractionMethod, Proxy
 from settings import TTL_PROXY
 
@@ -33,3 +34,31 @@ def test_create_proxies(session: Session, extraction_method: ExtractionMethod):
     proxies: list[Proxy] = session.execute(query).scalars().all()
 
     assert len(proxies) == len(_proxies)
+
+
+@freeze_time("2023-01-01 08:41:38")
+def test_proxy(session, mocker, extraction_method: ExtractionMethod):
+    mocker.patch(
+        "app.commands.proxy.ExtractionMethod.get_all_extraction_methods_sorted_by_priority",
+        return_value=[extraction_method],
+    )
+    mocker.patch(
+        "app.commands.proxy.search_method",
+        return_value=["1.1.1.1:8080"],
+    )
+    mocker.patch(
+        "app.commands.proxy.proxy_request",
+        return_value=True,
+    )
+
+    proxy(session)
+
+    query = select(Proxy)
+    proxies: list[Proxy] = session.execute(query).scalars().all()
+
+    assert len(proxies) == 1
+    assert proxies[0].port == 8080
+    assert proxies[0].status_check is True
+    assert proxies[0].ttl == TTL_PROXY
+    assert proxies[0].last_check == datetime.now()
+    assert proxies[0].extraction_method_id == extraction_method.id
